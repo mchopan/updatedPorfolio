d8d27588efcde5ddd88af4a05c6329d76782df7a
mchopan <mchopan21@gmail.com>
2024-01-27 16:34:49 +0530

    added spline for small screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,48 @@
 "use client"
-
 import React, { Suspense, useEffect, useState } from "react";
-
-import loader from "@/assets/loader.json"
 import Lottie from "lottie-react";
-
-const Spline = React.lazy(() => import('@splinetool/react-spline'));
-
-
+import loader from "@/assets/loader.json";
+import Spline from "@splinetool/react-spline";
 
 export default function Home() {
-
   const [isLoading, setIsLoading] = useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setIsLoading(false);
     }, 10000);
 
-    return () => clearTimeout(timeoutId);
-  }, []);
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
 
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      clearTimeout(timeoutId);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <>
-      {
-        isLoading ?
-          <div className="h-[calc(100vh-80px)] bg-[#ADA1EC] flex justify-center items-center">
-            <div className="w-[300px]">
-              <Lottie animationData={loader} loop={true} />
-            </div>
-          </div> : null
-      }
-      <div className={isLoading ? "hidden" : "visible bg-white text-slate-800 flex flex-shrink-0 h-[calc(100vh-80px)] justify-end "}>
+      {isLoading ? (
+        <div className="h-[calc(100vh-80px)] bg-[#ADA1EC] flex justify-center items-center">
+          <div className="w-[300px]">
+            <Lottie animationData={loader} loop={true} />
+          </div>
+        </div>
+      ) : null}
+
+      {/* Render based on screen size */}
+      <div className={isLoading ? "hidden" : `visible bg-white text-slate-800 flex flex-shrink-0 h-[calc(100vh-80px)] justify-end ${isSmallScreen ? "small-screen" : "big-screen"}`}>
         <div className="w-full">
-          <Suspense fallback={
-            <div className="font-extrabold size-11 text-blue-500">Loading</div>
-          }>
-            <Spline scene="https://prod.spline.design/HdgXOEvQ8kL8Ukkn/scene.splinecode" />
+          <Suspense fallback={<div className="font-extrabold size-11 text-blue-500">Loading</div>}>
+            <Spline scene={isSmallScreen ? "https://prod.spline.design/5YWMuDfT-o7lXYJA/scene.splinecode" : "https://prod.spline.design/HdgXOEvQ8kL8Ukkn/scene.splinecode"} />
           </Suspense>
         </div>
-      </div >
-
+      </div>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -25,37 +25,39 @@ export default function Header() {
 
     return (
         <div className="bg-purple-300 text-white h-auto p-2 flex justify-around items-center">
-            <div className="hidden md:flex">
+            <div className="">
                 <Link
                     href={'/'}
-                    className={`bg-purple-500 p-1 font-extrabold rounded hover:bg-purple-800 ${activeLink === 0 ? 'border-b-4' : ''}`}
+                    className={`p-1 rounded hover:bg-purple-800 ${activeLink === 0 ? 'border-b-4' : ''}`}
                     onClick={() => handleLinkClick(0)}
-                >M-CHOPAN</Link>
+                >
+                    M-CHOPAN
+                </Link>
             </div>
-            <div className="flex justify-between gap-4 flex-wrap">
+            <div className="flex justify-between gap-1 flex-wrap">
                 <Link
-                    className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 1 ? 'border-b-4' : ''}`}
+                    className={` p-1 rounded hover:bg-purple-800 ${activeLink === 1 ? 'border-b-4' : ''}`}
                     href={"/about"}
                     onClick={() => handleLinkClick(1)}
                 >
                     About
                 </Link>
                 <Link
-                    className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 2 ? 'border-b-4' : ''}`}
+                    className={` p-1 rounded hover:bg-purple-800 ${activeLink === 2 ? 'border-b-4' : ''}`}
                     href={"/skills"}
                     onClick={() => handleLinkClick(2)}
                 >
                     Skills
                 </Link>
                 <Link
-                    className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 3 ? 'border-b-4' : ''}`}
+                    className={`p-1 rounded hover:bg-purple-800 ${activeLink === 3 ? 'border-b-4' : ''}`}
                     href={"/projects"}
                     onClick={() => handleLinkClick(3)}
                 >
                     Projects
                 </Link>
                 <Link
-                    className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 4 ? 'border-b-4' : ''}`}
+                    className={` p-1 rounded hover:bg-purple-800 ${activeLink === 4 ? 'border-b-4' : ''}`}
                     href={"contact"}
                     onClick={() => handleLinkClick(4)}
                 >
@@ -64,7 +66,7 @@ export default function Header() {
                 <div className="hidden md:flex lg:">
                     {!user ? (
                         <Link
-                            className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 5 ? 'border-b-4' : ''}`}
+                            className={` p-1 rounded hover:bg-purple-800 ${activeLink === 5 ? 'border-b-4' : ''}`}
                             href={'/login'}
                             onClick={() => handleLinkClick(5)}
                         >
@@ -72,7 +74,7 @@ export default function Header() {
                         </Link>
                     ) : (
                         <Link
-                            className={`bg-purple-500 p-1 rounded hover:bg-purple-800 ${activeLink === 6 ? 'border-b-4' : ''}`}
+                            className={`p-1 rounded hover:bg-purple-800 ${activeLink === 6 ? 'border-b-4' : ''}`}
                             href={'/admin'}
                             onClick={() => handleLinkClick(6)}
                         >
