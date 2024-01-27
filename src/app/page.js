dda6b8f0d796d66d00725f5cb57e4be32e5b09b3
mchopan <mchopan21@gmail.com>
2024-01-27 16:42:10 +0530

    added spline for small screen

--- src/app/page.js
+++ src/app/page.js
@@ -9,22 +9,31 @@ export default function Home() {
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setIsLoading(false);
-    }, 10000);
-
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth <= 768);
     };
 
+    // Update state when component mounts on the client side
+    setIsSmallScreen(window.innerWidth <= 768);
+
+    // Attach event listener for window resize
     window.addEventListener("resize", handleResize);
 
+    // Cleanup event listener on component unmount
     return () => {
-      clearTimeout(timeoutId);
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 10000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
   return (
     <>
       {isLoading ? (
@@ -35,8 +44,7 @@ export default function Home() {
         </div>
       ) : null}
 
-      {/* Render based on screen size */}
-      <div className={isLoading ? "hidden" : `visible bg-white text-slate-800 flex flex-shrink-0 h-[calc(100vh-80px)] justify-end ${isSmallScreen ? "small-screen" : "big-screen"}`}>
+      <div className={`flex flex-shrink-0 justify-end h-[calc(100vh-90px)] bg-white text-slate-800 ${isSmallScreen ? "small-screen" : "big-screen"}`}>
         <div className="w-full">
           <Suspense fallback={<div className="font-extrabold size-11 text-blue-500">Loading</div>}>
             <Spline scene={isSmallScreen ? "https://prod.spline.design/5YWMuDfT-o7lXYJA/scene.splinecode" : "https://prod.spline.design/HdgXOEvQ8kL8Ukkn/scene.splinecode"} />
